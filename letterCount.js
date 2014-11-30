0934224df53ffee5bc605cdca05c29bd246f9656
Josh Andrews <jtandr3ws37@gmail.com>
2014-11-30 11:47:29 -0800

    letter counter function, correct answer.

---
+++ letterCount.js
@@ -0,0 +1,14 @@
+var letterCount = function (str) {
+	var obj = {}
+	for(var i = 0; i < str.length; i += 1) {
+		var letter = str[i];
+		if(letter in obj) {						//or if(!!(obj[letter]))
+			obj[letter] += 1;
+		} else {
+			obj[letter] = 1;
+		}
+	}
+	return obj;
+};
+
+console.log(letterCount('hello, world'));
